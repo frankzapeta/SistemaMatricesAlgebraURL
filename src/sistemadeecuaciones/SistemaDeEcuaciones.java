74d70ed1368e19ffa12c87711b37451a5ba42e49
EST1583708 <EST1583708@34bxb42.LaboratoriosCQ.com.gt>
2016-06-17 09:50:13 -0600

    Proyecto total

---
+++ src/sistemadeecuaciones/SistemaDeEcuaciones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadeecuaciones;
+
+/**
+ *
+ * @author EST1583708
+ */
+public class SistemaDeEcuaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
