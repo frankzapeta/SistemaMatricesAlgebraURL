dec1d524908298c4002f8a646efcf1f767a0441e
EST1583708 <EST1583708@34bxb42.LaboratoriosCQ.com.gt>
2016-06-17 09:53:21 -0600

    Cree una variable de tipo nodo

---
+++ src/sistemadeecuaciones/Matriz.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadeecuaciones;
+
+import objetos.Celda;
+
+/**
+ *
+ * @author EST1583708
+ */
+public class Matriz {
+    Celda nodo = new Celda();
+}
